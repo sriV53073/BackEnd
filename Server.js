5e1ed98f5799044942414b13d973727e6edf0a7a
Sri Vemugunta <svemugunta@gmail.com>
2021-08-23 03:45:25 -0500

    Done

---
+++ Server.js
@@ -0,0 +1,36 @@
+const nodemailer = require("nodemailer");
+var express = require('express');
+const app = express();
+var cors = require('cors');
+const port = 5000;
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'svemugunta@gmail.com',
+        pass: 'Srikris123'
+    }
+})
+app.post('/email', (req, res) =>{
+    const mailOptions = {
+        from: 'svemugunta@gmail.com',
+        to: 'svemugunta@gmail.com',
+        subject: `${req.body.content.email} sent you a message`,
+        text: req.body.content.subjectValue
+  }
+  console.log(mailOptions);
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          res.send({error: 'error'});
+        } else {
+          res.send({email: 'email Sent'});
+        }
+        res.end()
+      });
+})
+app.listen(port, () => {
+   console.log(`Your lstining on ${port}`)
+})
