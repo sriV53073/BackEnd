24f27f4b37fdcdbba1b5a8e5a7e468fdf874bbe9
Sri Vemugunta <svemugunta@gmail.com>
2021-08-23 04:45:43 -0500

    Done

--- Server.js
+++ Server.js
@@ -29,6 +29,10 @@ app.post('/email', (req, res) =>{
         res.end()
       });
 })
+app.get('/', function (req, res) {
+  res.send('GET request to the homepage1')
+  console.log("Hello");
+})
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
